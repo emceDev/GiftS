4ac971fc2e859b3afec58d019e3c5546e93885cc
m4t1ce <m4t1ce@gmail.com>
2020-10-28 18:54:38 +0100

    long time ago

--- src/App.js
+++ src/App.js
@@ -1,21 +1,85 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-import { Admin } from "./pages/Admin";
-import { Auth } from "./pages/Auth";
-import { Viewer } from "./pages/Viewer";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { ProtectedRoute } from "./components/PrivateRoute";
+import { Section } from "./components/Section";
+
+const data = [
+
+	{
+		'id':'0',
+		'text':'Opis obrazka ',
+		'img':'https://res.cloudinary.com/m4t1ce/image/upload/v1603814005/serafin/seraf_itiv1j.jpg',
+		'oC':'blue',
+		'tC':'white',
+	},	
+	{
+		'id':'1',
+		'text':'Purple overlay here Bardzo długi z resztą taki na testy w sam raz, a jadnak nie musi byc jeszcze dlyzszy',
+		'img':'https://res.cloudinary.com/m4t1ce/image/upload/v1603814004/serafin/bridge_jcktvp.jpg',
+		'oC':'purple',
+		'tC':'white',
+	},	
+	{
+		'id':'2',
+		'text':'Orange overlay here',
+		'img':'https://res.cloudinary.com/m4t1ce/image/upload/v1603814000/serafin/see2_b6litf.jpg',
+		'oC':'orange',
+		'tC':'white',
+	},	
+	{
+		'id':'3',
+		'text':'green overlay here',
+		'img':'https://res.cloudinary.com/m4t1ce/image/upload/v1603813995/serafin/forestSnow_mngk1v.jpg',
+		'oC':'green',
+		'tC':'white',
+	},	
+	{
+		'id':'4',
+		'text':'green overlay here',
+		'img':'https://res.cloudinary.com/m4t1ce/image/upload/v1603813995/serafin/sehen1_bjpopo.jpg',
+		'oC':'red',
+		'tC':'white',
+	},	
+	{
+		'id':'5',
+		'text':'green overlay here',
+		'img':'https://res.cloudinary.com/m4t1ce/image/upload/v1603813988/serafin/road1_u5dydl.jpg',
+		'oC':'yellow',
+		'tC':'white',
+	}
+]
 export const App = () => {
+	const [overCol, setoverCol] = useState('blue')
+	const [scrPos, setscrPos] = useState('0')
+	const [resized, setresized] = useState(false)
+
+	function inView(color) {
+		setoverCol(color)
+	}
+
+	function resi(){
+		setresized(true)
+		setresized(false)
+	}
+
+	useEffect(() => {
+		// console.log(window.pageYOffset)
+		window.addEventListener('scroll',(()=>{setscrPos(window.pageYOffset)}))
+		window.addEventListener('resize',resi)
+	}, [])
+
 	return (
 		<div className="App">
-			<Router>
-				<Link to="/Admin">Admin</Link>
-				<Link to="/Auth">Auth</Link>
-				<Link to="/Viewer">Viewer</Link>
-				<Route path="/Auth" component={Auth} />
-				<ProtectedRoute exact path="/Admin" component={Admin} />
-				<Route exact path="/Viewer" component={Viewer} />
-			</Router>
+			<div>
+				<div style={{position:"fixed",top:'0',left:'0',width:'100vw',height:'100vh',opacity:'0.3',zIndex:'3',transition:'background-color 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s',backgroundColor:overCol}}>
+				</div>
+
+				{data.map((x)=>{
+					return(
+						<Section  id={x.id} resized={resized} inView={inView} scrPos={scrPos} img={x.img} text={x.text} oC={x.oC} tC={x.tC}/>
+					)
+				})}
+				<div style={{position:'absolute',zIndex:'565', top:'0',left:"0"}}> woah I'ma Div</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/components/Section.js
@@ -0,0 +1,54 @@
+import React, {useState, useEffect } from 'react'
+import '../styles/Section.css'
+
+
+export const Section = props =>{
+    const [ImgH, setImgH] = useState('100vh')
+    const [imgLoad, setimgLoad] = useState(false)
+    function res(e){
+        if(e!==undefined){
+            setImgH(e.target.height)
+        }else{
+            let elmnts = document.getElementsByClassName('Section')
+            let imgH = elmnts[props.id].getElementsByTagName('img')[0]
+            setImgH(imgH.height)
+        }
+        
+    }
+    
+    useEffect(() => {
+        res()
+    }, [props.resized])
+
+    useEffect(()=>{
+        let Section = document.getElementsByClassName('Section')[props.id]
+        let bound = Section.getClientRects()[0]
+        console.log('Scrolling')
+        if(100 > bound.top && bound.bottom > 100){
+            props.inView(props.oC)
+            console.log(props.id +" SectionBot:  "+ bound.bottom+ "  SectionTop: "+bound.top)
+            console.log(ImgH)
+            var el = Section.getElementsByTagName('div')[0]
+            if(-bound.top > ImgH/2){
+                console.log(props.id,"  ",'Moving out...')
+                el.className = 'moved'
+                let amount = ImgH-ImgH/2-bound.bottom
+                console.log("amount"+amount)
+                el.style.transform = 'translateY('+amount*2+'px)'
+                // el.style.transform = 'translate(0px,'+ImgH+'px)'
+            }else if(bound.bottom === ImgH){
+                console.log('Backe')
+            }
+            // console.log("In VIew "+props.id)
+        }
+    },[props.scrPos])
+
+    return (
+        <div className="Section" style={{opacity:imgLoad ?'1':'0' }}>
+            <img onLoad={(e)=>{res(e);setimgLoad(true)}} src={props.img}></img>
+            <div style={{ bottom:ImgH/2 }}>
+               <p style={{color:props.tC}}>{props.text}</p>
+            </div>
+        </div>
+    )
+}
